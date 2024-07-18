5a102f0925dce50c455ebc5b69e22e88c294588a
muratcnshn24 <127681337+muratcnshn24@users.noreply.github.com>
2024-07-18 17:23:03 +0300

    Proje dosyası ekle.

---
+++ Aralikli_Faktoriyel_Hesabi/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Aralikli_Faktoriyel_Hesabi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 2-8
+
+            int t = 0;
+            for (int i = 2; i < 9; i++)
+            {
+                t += Matematik.Faktoriyel(i);
+                Console.WriteLine("{0}!={1}", i, Matematik.Faktoriyel(i) );
+            }
+
+            Console.WriteLine("Ortalama={0:F2}", (double)t/7);
+             
+        }
+    }
+}
